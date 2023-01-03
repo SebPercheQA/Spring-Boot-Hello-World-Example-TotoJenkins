58e1a662bf526ddb8008214491d17528f39d92b1
SebPercheQA <120563769+SebPercheQA@users.noreply.github.com>
2023-01-03 16:45:37 +0100

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -1,22 +1,51 @@
-package com.reytech.demo;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import com.reytech.demo.Personne;
+// package com.reytech.demo;
+// import static org.junit.jupiter.api.Assertions.assertAll;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import com.reytech.demo.Personne;
 
-@SpringBootTest
-class DemoApplicationTests {
+// @SpringBootTest
+// class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 	
-	@Test
-        void groupedAssertions() {
-        Personne pers = new Personne("Nassur", "Moumadi");
+// 	@Test
+//         void groupedAssertions() {
+//         Personne pers = new Personne("Nassur", "Moumadi");
 
-        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
-                              () -> assertEquals("Moumadi", pers.getLastName()));
+//         assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+//                               () -> assertEquals("Moumadi", pers.getLastName()));
+//     }
+// }
+
+package com.reytech.demo;
+
+public class Personne {
+
+    public String firstName;
+    public String lastName;
+
+    public Personne(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
